f0daa5c6e1cc1b07b2578d72d6e9639a69165688
Pandanym <nympanda@gmail.com>
2020-03-21 00:19:28 +0100

    basic position networking

---
+++ Assets/Scripts/Networking/NetworkedGameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class NetworkedGameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        NetworkManager.Instance.InstantiateNetworkedPlayer();
+    }
+}
