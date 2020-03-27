b8e80f773d1e989ccd7d11dda8f61a502eb7d7ef
Nicolas <nicoceriani@gmail.com>
2020-03-27 17:58:13 +0100

    Altar and Shrine Spawn

--- Assets/Scripts/CTF/FlagZoneSpawn.cs
+++ Assets/Scripts/CTF/FlagZoneSpawn.cs
@@ -6,4 +6,20 @@ public class FlagZoneSpawn : MonoBehaviour
 {
     public int teamIndex;
     public FlagZoneType type;
+
+    private void OnDrawGizmos()
+    {
+        
+        if(teamIndex == 0)
+        {
+            Gizmos.color = new Color(1, 0.23f, 0.37f, 1f);
+        }
+
+        else
+        {
+            Gizmos.color = new Color(0.14f, 0.65f, 1f, 1f);
+        }
+
+        Gizmos.DrawWireSphere(transform.position, 8f);
+    }
 }
