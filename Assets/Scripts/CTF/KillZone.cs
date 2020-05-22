763dd8608d4206cb3f60c50647b73eb64fe4f1d5
Nicolas <nicoceriani@gmail.com>
2020-05-22 20:00:02 +0200

    Fixed flag visuals appearing when falling in the void

--- Assets/Scripts/CTF/KillZone.cs
+++ Assets/Scripts/CTF/KillZone.cs
@@ -12,9 +12,27 @@ public class KillZone : MonoBehaviour
         Character chara;
         if (other.TryGetComponent(out chara))
         {
+            NetworkedPlayer p = chara.NPlayer;
+            if (!p) return;
+
+            if (!p.isAlive) return;
             Debug.Log(chara.PlayerName + " died in zone " + gameObject.name);
-            chara.NPlayer.networkObject.SendRpc(NetworkedPlayerBehavior.RPC_DIE, Receivers.All, "VOID", 0);
-            chara.Die();
+
+            
+            p.networkObject.alive = false;
+
+            p.networkObject.SendRpc(NetworkedPlayerBehavior.RPC_DIE, Receivers.All, "VOID", 0);
+
+            // return flag
+
+            if (p.flag == null) return;
+
+            p.flag.GetComponentInParent<Zone>().networkObject.SendRpc(Zone.RPC_RETRIEVED, Receivers.All, "VOID", p.playerName);
+
+            p.flag = null;
+            p.networkObject.hasFlag = false;
+
+            p.networkObject.SendRpc(NetworkedPlayer.RPC_TOGGLE_FLAG, true, Receivers.AllBuffered, false);
         }
     }
 }
